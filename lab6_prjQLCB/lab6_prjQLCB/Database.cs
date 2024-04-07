3129212508eb3f2291b6f7433d3fd582c1f00b18
Mee18 <nguyenseoul18@gmail.com>
2024-04-07 16:57:06 +0700

    Add class connect to SQL SERVER database using ADO.NET

---
+++ lab6_prjQLCB/lab6_prjQLCB/Database.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Database
+{
+    public static SqlConnection con;
+
+    public static bool OpenConnection()
+    {
+        try
+        {
+            con = new SqlConnection("Server=MEE\\MYB2014996;database=QLCB;uid=mylogin;pwd=mylogin");
+            con.Open();
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public static bool CloseConnection()
+    {
+        try
+        {
+            con.Close();
+        }
+        catch(Exception) {
+            return false;
+        }
+        return true;
+    }
+}
