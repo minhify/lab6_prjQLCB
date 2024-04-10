ef0ce89fdaf9c69eb6e2c16844733da326faf2aa
Mee18 <nguyenseoul18@gmail.com>
2024-04-10 21:31:01 +0700

    add ChuVu Controller

---
+++ lab6_prjQLCB/lab6_prjQLCB/ChucVuController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace lab6_prjQLCB
+{
+    class ChucVuController
+    {
+        public List<ChucVuModel.ChucVu> GetChucVu()
+        {
+            List<ChucVuModel.ChucVu> listChucVu = new List<ChucVuModel.ChucVu>();
+
+            try
+            {
+                if (!Database.OpenConnection())
+                    throw new Exception("Error: Unable to open database connection");
+
+                string query = "SELECT MaCV, TenCV FROM ChucVu";
+                SqlCommand command = new SqlCommand(query, Database.con);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    ChucVuModel.ChucVu chucVu = new ChucVuModel.ChucVu();
+                    chucVu._maCV = reader.GetInt32(0);
+                    chucVu._tenCV = reader.GetString(1);
+                    listChucVu.Add(chucVu);
+                }
+                Database.CloseConnection();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error: " + ex.Message);
+            }
+            return listChucVu;
+        }
+    }
+}
