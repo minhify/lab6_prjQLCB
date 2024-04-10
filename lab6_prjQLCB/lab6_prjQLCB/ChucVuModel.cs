4840c9d28383fd24cb2a4f839d723498ea457955
Mee18 <nguyenseoul18@gmail.com>
2024-04-10 21:29:25 +0700

    add ChucVu Model

---
+++ lab6_prjQLCB/lab6_prjQLCB/ChucVuModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6_prjQLCB
+{
+    class ChucVuModel
+    {
+        public class ChucVu
+        {
+            public int _maCV {  get; set; }
+            public string _tenCV {  get; set; }
+        }
+    }
+}
